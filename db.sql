70e118c5beabf1d76d15ba7005b408575822eec7
BruceZhao <BruceZhaoL@foxmail.com>
2018-03-11 23:19:28 +0800

    first comit

---
+++ db.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.5.40 : Database - how2java
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`how2java` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `how2java`;
+
+/*Table structure for table `product_` */
+
+DROP TABLE IF EXISTS `product_`;
+
+CREATE TABLE `product_` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(30) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `product_` */
+
+insert  into `product_`(`id`,`NAME`,`price`) values (1,'产品a',88.88),(2,'产品b',88.88),(3,'产品c',88.88),(4,'产品x',88.88),(5,'产品y',88.88),(6,'产品z',88.88);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
